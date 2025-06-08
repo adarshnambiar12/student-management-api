a33ad1be854e202b131e1eeb5134af6aa8f02a68
Adarsh Nambiar <adarshnambiar4912@gmail.com>
2025-06-08 13:01:20 +0530

    Change Backend API Url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://student-management-api-bd9u.onrender.com';
 
 // Configure axios
 axios.interceptors.request.use(
